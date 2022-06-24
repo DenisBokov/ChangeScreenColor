66933e3072c35ad6f6f74f515981b70f0efcfddf
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-06-24 12:18:23 +0300

    Initial Commit

---
+++ HomeWork2.03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2.03
+//
+//  Created by Denis Bokov on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
